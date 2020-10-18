a3ad174b05586737ba325c3599d7d019fce8480e
Caragu <ncaro.ag@gmail.com>
2020-10-18 18:40:46 -0500

    Se modifica script

--- bin/main/db/migration/V2.1__script_dml.sql
+++ bin/main/db/migration/V2.1__script_dml.sql
@@ -1,6 +1,6 @@
 INSERT INTO `city` (`id`, `name`) VALUES (NULL, 'BOGOTA');
 INSERT INTO `city` (`id`, `name`) VALUES (NULL, 'CALI');
-INSERT INTO `cinema` (`id`, `name`, `address`, `phone`, `id_city`) VALUES (NULL, 'UNICENTRO', 'Ak. 15 #124-30, Bogotá, Cundinamarca', '7789975', '1'):
+INSERT INTO `cinema` (`id`, `name`, `address`, `phone`, `id_city`) VALUES (NULL, 'UNICENTRO', 'Ak. 15 #124-30, Bogotá, Cundinamarca', '7789975', '1');
 INSERT INTO `cinema` (`id`, `name`, `address`, `phone`, `id_city`) VALUES (NULL, 'PALMETTO PLAZA', 'Cl. 9 #48-81, Cali, Valle del Cauca', '8824565', '2');
 INSERT INTO `person` (`id`, `name`, `lastname`, `address`, `phone`, `mail`) VALUES (NULL, 'KELLY', 'VASCO', 'CALLE 15A 23-55', '3124578986', 'KELLY@GMAIL.COM');
 INSERT INTO `theater` (`id`, `total_capacity`, `id_movie`) VALUES (NULL, '20', '1');
